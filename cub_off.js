8ad1e564ac4342a44b9807ac6be0691463a4048b
endLoads <166923792+endLoads@users.noreply.github.com>
2025-12-09 12:36:56 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -13,13 +13,14 @@
     };
     window.lampa_settings = _cleanSettings;
 
-    // 2. CSS
+    // 2. CSS (Скрываем визуально)
     function injectCleanerCSS() {
         var style = document.createElement("style");
         style.innerHTML = `
             .ad-server, [data-component="ad"], .card-promo, .button--subscribe, 
             .settings--account-premium, .open--notice, .selectbox-item__lock 
             { display: none !important; }
+
             .player-advertising, #oframe_player_advertising, .layer--advertising,
             .ad-preroll-container, div[class*="advertising"]
             {
@@ -30,35 +31,43 @@
         document.body.appendChild(style);
     }
 
-    // 3. УМНЫЙ ВЗЛОМ ВРЕМЕНИ (Smart Time Killer)
-    // Ускоряет таймеры ТОЛЬКО если реклама активна
+    // 3. ВЗЛОМ ТАЙМЕРОВ (TIME KILLER) - ГЛАВНОЕ ИСПРАВЛЕНИЕ
     function hackTimeouts() {
+        console.log('[TimeKiller] Перехват таймеров запущен');
+        
         var originalSetTimeout = window.setTimeout;
         
         window.setTimeout = function(func, delay) {
-            // Проверяем, есть ли сейчас на экране признаки рекламы
-            // (даже скрытой через CSS)
-            var adExists = document.querySelector('.player-advertising, .layer--advertising, #oframe_player_advertising');
-            
-            // Ускоряем ТОЛЬКО если есть реклама И задержка похожа на рекламную (3-15 сек)
-            if (adExists && delay > 2000 && delay < 16000) {
-                console.log('[TimeKiller] Реклама обнаружена! Ускоряем таймер ' + delay + 'ms -> 1ms');
-                return originalSetTimeout(func, 1);
+            // Если таймер ставится на 3-7 секунд (типичное ожидание рекламы)
+            if (delay > 2500 && delay < 7000) {
+                console.log('[TimeKiller] Найден подозрительный таймер (' + delay + 'ms) -> Ускоряем до 1ms');
+                // Заменяем задержку на 1 миллисекунду (мгновенно)
+                return originalSetTimeout(func, 1); 
             }
-            
-            // В остальных случаях (интерфейс плеера) - работаем штатно
+            // Все остальные таймеры работают как обычно
             return originalSetTimeout(func, delay);
         };
     }
 
-    // 4. СЕТЬ
+    // 4. СЕТЕВОЙ БЛОК (Чтобы реклама падала в ошибку сразу)
     function startNetworkInterceptor() {
         var blockList = ['vast', 'preroll', 'advertising', 'yandex.ru/ads', 'googleads'];
+        
+        var originalFetch = window.fetch;
+        window.fetch = function(input, init) {
+            var url = (typeof input === 'string') ? input : (input.url || '');
+            for (var i = 0; i < blockList.length; i++) {
+                if (url.indexOf(blockList[i]) !== -1) return Promise.reject('AdBlocked');
+            }
+            return originalFetch.apply(this, arguments);
+        };
+        
         var originalOpen = XMLHttpRequest.prototype.open;
         XMLHttpRequest.prototype.open = function (method, url) {
             if (typeof url === 'string') {
                 for (var i = 0; i < blockList.length; i++) {
                     if (url.indexOf(blockList[i]) !== -1) {
+                        // Важно: не просто блокируем, а вызываем ошибку, чтобы сработал таймер восстановления
                         this.onerror = function() {}; 
                         return originalOpen.call(this, method, 'http://0.0.0.0'); 
                     }
@@ -68,25 +77,32 @@
         };
     }
 
-    // 5. ПРИНУДИТЕЛЬНЫЙ ПУСК (Более аккуратный)
+    // 5. ПРИНУДИТЕЛЬНЫЙ СТАРТ ПЛЕЕРА
     function forcePlay() {
-        // Проверяем раз в секунду, не зависло ли всё на рекламе
         setInterval(function() {
-            var adLayer = $('.player-advertising, .layer--advertising');
-            if (adLayer.length) {
+            // Если видим слой рекламы
+            if ($('.player-advertising, .layer--advertising').length) {
+                // Пытаемся сообщить Лампе, что реклама кончилась
                 if (Lampa.Player && Lampa.Player.trigger) Lampa.Player.trigger('ad_end');
-                adLayer.remove();
+                
+                // Удаляем слой
+                $('.player-advertising, .layer--advertising').remove();
             }
             
-            // НЕ трогаем video.play(), если пользователь сам нажал паузу
-            // (определяем это: если нет рекламного слоя, то не вмешиваемся)
-        }, 1000); 
+            // Если видео есть, но стоит на паузе в начале (0 сек)
+            var video = $('video')[0];
+            if (video && video.paused && video.currentTime < 1) {
+                // Проверяем, не нажимал ли пользователь паузу сам. 
+                // Обычно реклама блокирует воспроизведение. Пытаемся пустить.
+                video.play().catch(function(e){});
+            }
+        }, 500);
     }
 
     function startPlugin() {
         localStorage.setItem("region", JSON.stringify({code: "uk", time: new Date().getTime()}));
         injectCleanerCSS();
-        hackTimeouts();
+        hackTimeouts();          // <--- Включаем перехват времени
         startNetworkInterceptor();
         forcePlay();
     }
