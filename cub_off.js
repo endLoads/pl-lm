c264124db8f76c42fa8e53a5a5fb514531fd1e81
endLoads <166923792+endLoads@users.noreply.github.com>
2025-12-09 12:55:02 +0300

    Update cub_off.js

--- cub_off.js
+++ cub_off.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var PLUGIN_VERSION = 'CUB OFF v4.1 (Event Boost)';
+    var PLUGIN_VERSION = 'CUB OFF v5.0 (Direct Kill)';
 
     // 1. КОНФИГУРАЦИЯ
     var _cleanSettings = {
@@ -15,13 +15,16 @@
     };
     window.lampa_settings = _cleanSettings;
 
-    // 2. CSS
+    // 2. CSS (Скрываем визуально)
     function injectCleanerCSS() {
         var style = document.createElement("style");
         style.innerHTML = `
-            .ad-server, [data-component="ad"], .card-promo, .button--subscribe, 
+            .ad-server, [data-component="ad"], .card-promo, 
             .settings--account-premium, .open--notice, .selectbox-item__lock 
             { display: none !important; }
+            .button--subscribe { display: none !important; }
+
+            /* Делаем рекламу невидимой, но оставляем в потоке, чтобы JS её нашел */
             .player-advertising, #oframe_player_advertising, .layer--advertising,
             .ad-preroll-container, div[class*="advertising"]
             {
@@ -37,72 +40,64 @@
         document.body.appendChild(style);
     }
 
-    // 3. УСКОРИТЕЛЬ ПО СОБЫТИЯМ (Event-based Boost)
-    var isBoostActive = false; // По умолчанию ВЫКЛЮЧЕН
-    var boostTimer = null;
-
-    function activateBoost() {
-        // console.log('[Boost] АКТИВИРОВАН на 15 сек');
-        isBoostActive = true;
-        
-        if(boostTimer) clearTimeout(boostTimer);
-        
-        // Выключаем через 15 секунд (с запасом)
-        // Используем хакнутый setTimeout, но он сам себя не ускорит, т.к. задержка 15000 (а мы хакаем < 7000)
-        // На всякий случай используем нестандартное число 15500
-        var originalSetTimeout = window.setTimeout; // Берем оригинал, если он еще доступен в замыкании
-        
-        boostTimer = originalSetTimeout(function() { 
-            // console.log('[Boost] ОТКЛЮЧЕН');
-            isBoostActive = false; 
-        }, 15500);
+    // 3. БЛОКИРОВКА НА УРОВНЕ ЯДРА (Без таймеров)
+    function patchLampaCore() {
+        // Подмена модуля рекламы
+        if (Lampa.Ad) {
+            Lampa.Ad.launch = function (data) {
+                // Сразу говорим "реклама прошла"
+                if (data && data.callback) data.callback();
+            };
+        }
     }
 
-    function hackTimeouts() {
-        var originalSetTimeout = window.setTimeout;
-        
-        window.setTimeout = function(func, delay) {
-            // УСЛОВИЕ:
-            // 1. Ускоритель включен (мы нажали плей недавно)
-            // 2. Задержка похожа на рекламную (от 2 до 8 секунд)
-            if (isBoostActive && delay > 2000 && delay < 8000) {
-                // console.log('[Boost] Skip ' + delay + 'ms');
-                return originalSetTimeout(func, 1); 
-            }
-            return originalSetTimeout(func, delay);
-        };
-
-        // Слушаем старт плеера, чтобы включить буст
+    // 4. АГРЕССИВНЫЙ КИЛЛЕР ПРИ СТАРТЕ
+    function setupKiller() {
+        // Слушаем запуск плеера
         Lampa.Listener.follow('player', function(e) {
             if(e.type === 'start' || e.type === 'play') {
-                activateBoost();
+                // Запускаем "бешеный" цикл на 5 секунд
+                var attempts = 0;
+                var interval = setInterval(function() {
+                    attempts++;
+                    
+                    // 1. Ищем рекламные слои
+                    var ads = $('.player-advertising, .layer--advertising, #oframe_player_advertising');
+                    
+                    if (ads.length > 0) {
+                        // Нашли! Удаляем!
+                        ads.remove();
+                        // Сообщаем плееру, что всё ок
+                        if (Lampa.Player.trigger) Lampa.Player.trigger('ad_end');
+                        
+                        // Если видео стоит на паузе (из-за рекламы) - включаем
+                        var video = $('video')[0];
+                        if (video && video.paused) {
+                            try { video.play(); } catch(err){}
+                        }
+                    }
+
+                    // 2. Ищем кнопки "Пропустить"
+                    $('.skip-button, .ad-skip-button').click();
+
+                    // Останавливаем через 500 попыток (около 5 сек)
+                    if (attempts > 500) clearInterval(interval);
+                    
+                }, 10); // Проверка каждые 10мс (очень быстро)
             }
-        });
-        
-        // Также слушаем событие "реклама", если оно проскочит
-        Lampa.Listener.follow('ad', function(e) {
-             activateBoost();
         });
     }
 
-    // 4. СЕТЕВОЙ ПЕРЕХВАТЧИК
+    // 5. СЕТЕВОЙ ПЕРЕХВАТЧИК (Для надежности)
     function startNetworkInterceptor() {
         var blockList = ['vast', 'preroll', 'advertising', 'yandex.ru/ads', 'googleads'];
-        var originalFetch = window.fetch;
-        window.fetch = function(input, init) {
-            var url = (typeof input === 'string') ? input : (input.url || '');
-            for (var i = 0; i < blockList.length; i++) {
-                if (url.indexOf(blockList[i]) !== -1) return Promise.reject('AdBlocked');
-            }
-            return originalFetch.apply(this, arguments);
-        };
         var originalOpen = XMLHttpRequest.prototype.open;
         XMLHttpRequest.prototype.open = function (method, url) {
             if (typeof url === 'string') {
                 for (var i = 0; i < blockList.length; i++) {
                     if (url.indexOf(blockList[i]) !== -1) {
                         this.onerror = function() {}; 
-                        return originalOpen.call(this, method, 'http://0.0.0.0'); 
+                        return originalOpen.call(this, method, 'about:blank'); 
                     }
                 }
             }
@@ -110,18 +105,7 @@
         };
     }
 
-    // 5. DOM CLEANER
-    function forcePlay() {
-        setInterval(function() {
-            var adLayer = $('.player-advertising, .layer--advertising');
-            if (adLayer.length) {
-                if (Lampa.Player && Lampa.Player.trigger) Lampa.Player.trigger('ad_end');
-                adLayer.remove();
-            }
-        }, 500);
-    }
-
-    // 6. VERSION UI
+    // 6. UI
     function injectInfo() {
         var observer = new MutationObserver(function(mutations) {
             var settingsBox = document.querySelector('.settings__content');
@@ -138,9 +122,9 @@
     function startPlugin() {
         localStorage.setItem("region", JSON.stringify({code: "uk", time: new Date().getTime()}));
         injectCleanerCSS();
-        hackTimeouts();          
+        patchLampaCore();        // <--- Патч ядра вместо таймеров
+        setupKiller();           // <--- Быстрый киллер при старте
         startNetworkInterceptor();
-        forcePlay();
         injectInfo();
     }
 
